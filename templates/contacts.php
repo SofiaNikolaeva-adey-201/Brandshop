5a681f4fef2cac5aeb05ca627044752e2dc66b61
MarinaSafina <72244701+MarinaSafina@users.noreply.github.com>
2022-05-03 15:58:04 +0300

    Update contacts.php

--- templates/contacts.php
+++ templates/contacts.php
@@ -1,21 +1,22 @@
 <div id="contact_summary">
     <h1 id="contactText">
-        Делаем заголовок
+        Наши контакты
     </h1>
 </div>
 
 <div id="our_contacts">
     <div id="contacts1">
-        мб телефон
+        Для звонков по России: 8 800 555 00 00
+        Горячая линия: 8 900 753 35 77
     </div>
     <div id="contacts2">
-        можно адрес
+        Наш адрес: Москва, проспект Мира, 1, офис 304
     </div>
     <div id="contacts3">
-        если вставишь гугл карту, будешь ваще красоткой
+        https://yandex.ru/maps/-/CCUFF2Be2D
     </div>
     <div id="contacts4">
-        почта
+        Адрес электронной почты: info@brandshop.ru
     </div>
     <div id="contacts5">
         ссылка на инст? 
