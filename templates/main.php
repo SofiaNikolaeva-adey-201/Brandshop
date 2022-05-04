8cbd1b170fe55d056b68c8beec539f1faa8c6d47
Александра Иофчу <80959973+IofchuSasha@users.noreply.github.com>
2022-05-04 16:54:10 +0300

    Update main.php
    Добавила текс на главную страницу с описанием магазина

--- templates/main.php
+++ templates/main.php
@@ -1,11 +1,18 @@
 <div id="promo">
     <h1 id="promoText">
-        Новый PhoneBrand - это лучший смартфон
+        Новый PhoneBrand - это лучший телефон
     </h1>
 </div>
 
 <div id="mainTextWrap">
     <div id="mainText">
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+Мы российская компания, предоставляющая лучшие  мобильные телефоны на рынке. 
+
+Здесь Вы можете подобрать на свой вкус одну из новейших моделей представленных на данный  момент.
+
+Наша компания несет ответственность за качество товаров, поэтому сотрудничает только с официальными представителями Apple, Samsung и HTC.
+
+Мы-это качество
+
     </div>
 </div>
